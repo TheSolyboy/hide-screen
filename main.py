7e82e1f66517007f1dda82f68b95314d19ce3ab9
SolyNex <p232e.eeee@gmail.com>
2025-04-11 08:36:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ import tkinter as tk
 import keyboard
 import threading
 
-def __init__(self):
-    self.root = tk.Tk()
-    self.root.attributes("-fullscreen", True)
-    self.root.config(bg="black")
-    self.root.overrideredirect(True)
-    self.root.geometry("1920x1080+0+0")
-    self.overlay_visible = False
-
-    self.hotkey_thread = threading.Thread(target=self.listen_for_hotkey)
-    self.hotkey_thread.daemon = True
-    self.hotkey_thread.start()
-
-def toggle_overlay(self):
-    if self.overlay_visible:
-        self.root.withdraw()
+class BlackScreenOverlay:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.attributes("-fullscreen", True)
+        self.root.config(bg="black")
+        self.root.overrideredirect(True)
+        self.root.geometry("1920x1080+0+0")
         self.overlay_visible = False
-    else:
-        self.root.deiconify()
-        self.overlay_visible = True
 
-def listen_for_hotkey(self):
-    keyboard.add_hotkey('ctrl+shift+b', self.toggle_overlay)
+        self.hotkey_thread = threading.Thread(target=self.listen_for_hotkey)
+        self.hotkey_thread.daemon = True
+        self.hotkey_thread.start()
 
-def run(self):
-    self.root.mainloop()
+    def toggle_overlay(self):
+        if self.overlay_visible:
+            self.root.withdraw()
+            self.overlay_visible = False
+        else:
+            self.root.deiconify()
+            self.overlay_visible = True
+
+    def listen_for_hotkey(self):
+        keyboard.add_hotkey('ctrl+shift+b', self.toggle_overlay)
+
+    def run(self):
+        self.root.mainloop()
+
+if __name__ == "__main__":
+    black_screen = BlackScreenOverlay()
+    black_screen.run()
